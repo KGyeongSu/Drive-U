a647e6fe6e4e15f2d43f9a2e8d07062bab7fbe9e
KGyeongSu <ggp2876064@gmail.com>
2026-05-14 12:33:15 +0900

    chore: init project setup(Spring Boot 3.5.14)

---
+++ src/main/java/com/zerock/driveu/DriveuApplication.java
@@ -0,0 +1,13 @@
+package com.zerock.driveu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriveuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerock/driveu/DriveuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerock.driveu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
